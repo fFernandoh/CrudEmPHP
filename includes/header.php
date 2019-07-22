581c7fd42b4b75f4f10ac3d89000d75780d5b830
Fernando <loja.info.f.h@gmail.com>
2019-07-22 15:59:53 -0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- CSS MATERIALISE -->
+    <link rel="stylesheet" href="materialize/css/materialize.min.css">
+
+    <title>teste</title>
+</head>
+<body>
