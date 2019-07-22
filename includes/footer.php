46f01eaabf848d9391dbc0d19089fac8d0d4a06b
Fernando <loja.info.f.h@gmail.com>
2019-07-22 15:59:51 -0300

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+ <!--JQUERY  e JAVASCRIPT -->
+ <script type="text/javascript" src="materialize/js/jquery-3.4.1.min.js"></script>
+ <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+
+ <!-- inicialização jquery -->
+ <script type="text/javascript">
+    $(document).ready(function(){
+
+    });
+    </script>
+</body>
+</html>
